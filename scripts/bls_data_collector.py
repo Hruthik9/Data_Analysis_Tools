57416d4348d02d63721591aee1ac49279d9bd511
Hruthik9 <121885424+Hruthik9@users.noreply.github.com>
2024-12-11 21:24:01 +0530

    Update bls_data_collector.py

--- scripts/bls_data_collector.py
+++ scripts/bls_data_collector.py
@@ -5,10 +5,32 @@ from datetime import datetime
 
 # Constants
 API_KEY = "758c2e749d7f42ff838022c0adea1470"  # Replace with your BLS API key
-SERIES_IDS = ["CEU0000000001", "LNS14000000","LNS11300000","LNS12300000","LNS12032194"]
-# Non-farm workers, unemployment rate,Labor Force Participation Rate,Civilian Employment-Population Ratio,Part-Time for Economic Reasons
-START_YEAR = "2022"
+SERIES_IDS = ['CES0000000001', 'CES0500000001', 'CES2000000001', 'CES3000000001',
+              'CES4000000001', 'CES6000000001', 'CES7000000001', 'CES8000000001',
+              'LNS14000000', 'LNS11300000', 'LNS12300000', 'LNS13023621',
+              'LNS12032194', 'LNS12500000', 'LNS12600000']
+# Non-farm workers, unemployment rate, and more
+START_YEAR = "2000"
 END_YEAR = str(datetime.now().year)
+
+# Mapping series IDs to descriptive names
+SERIES_ID_NAME_MAP = {
+    "CES0000000001": "Total Nonfarm Employment",
+    "CES0500000001": "Total Private Employment",
+    "CES2000000001": "Construction Employment",
+    "CES3000000001": "Manufacturing Employment",
+    "CES4000000001": "Trade, Transportation, and Utilities Employment",
+    "CES6000000001": "Professional and Business Services Employment",
+    "CES7000000001": "Education and Health Services Employment",
+    "CES8000000001": "Leisure and Hospitality Employment",
+    "LNS14000000": "Unemployment Rate",
+    "LNS11300000": "Labor Force Participation Rate",
+    "LNS12300000": "Civilian Employment-Population Ratio",
+    "LNS13023621": "Part-Time for Economic Reasons",
+    "LNS12032194": "Women Labor Force Participation",
+    "LNS12500000": "Median Duration of Unemployment",
+    "LNS12600000": "Average Hourly Earnings"
+}
 
 def fetch_bls_data(series_ids, start_year, end_year, api_key):
     """Fetch data from BLS API."""
